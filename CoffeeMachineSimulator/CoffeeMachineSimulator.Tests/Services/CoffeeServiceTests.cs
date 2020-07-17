628eb901df2c2fb8cb049f775f105d9ef6cf4d0c
Martisca Paul <martisca.a.paul@gmail.com>
2020-07-17 14:02:23 +0300

    Last Update by Trainer

---
+++ CoffeeMachineSimulator/CoffeeMachineSimulator.Tests/Services/CoffeeServiceTests.cs
@@ -0,0 +1,43 @@
+﻿using CoffeeMachineSimulator.Services.Models;
+using CoffeeMachineSimulator.Services.Services;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CoffeeMachineSimulator.Tests.Services
+{
+    [TestFixture]
+    public class CoffeeServiceTests
+    {
+        private CoffeeService coffeeService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            coffeeService = new CoffeeService();
+        }
+
+        [Test]
+        public void GetCoffees_Returns_ListOfCoffeeModels()
+        {
+            var coffeesReturned = coffeeService.GetCoffees();
+
+            Assert.IsNotNull(coffeesReturned);
+            Assert.IsTrue(coffeesReturned.Any());
+            Assert.IsInstanceOf(typeof(List<CoffeeModel>), coffeesReturned);
+            Assert.IsTrue(coffeesReturned.Any(coffeeModel => coffeeModel.Id != Guid.Empty && !string.IsNullOrEmpty(coffeeModel.Name)));
+        }
+
+        [Test]
+        public void AddNullCoffee_Throws_NewException()
+        {
+            var ex = Assert.Throws<Exception>(() => coffeeService.AddCoffee(null));
+            
+            Assert.AreEqual(ex.Message, "You should not add null entries!");
+        }
+
+        //TODO: Add Unit test for Remaining add cases
+        //Todo: Add Unit test for deleting case
+    }
+}
