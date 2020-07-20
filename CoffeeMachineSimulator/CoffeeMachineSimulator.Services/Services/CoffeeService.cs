511b85c20e3556a2a78827f8406dcc7bc6a14b75
Sebastian Balcanu <sebi_balcanu@yahoo.com>
2020-07-20 18:03:55 +0300

    Added unit tests for remaining cases Added NBuilder package

--- CoffeeMachineSimulator/CoffeeMachineSimulator.Services/Services/CoffeeService.cs
+++ CoffeeMachineSimulator/CoffeeMachineSimulator.Services/Services/CoffeeService.cs
@@ -13,24 +13,39 @@ namespace CoffeeMachineSimulator.Services.Services
 
         public CoffeeService()
         {
-            coffeeModels = Builder<CoffeeModel>.CreateListOfSize(10).Build().ToList();
+            coffeeModels = Builder<CoffeeModel>.CreateListOfSize(10)
+                .TheFirst(1)
+                .With(x=>x.Name = "Lavazza")
+                .Build()
+                .ToList();
         }
 
         public void AddCoffee(CoffeeModel coffeeToAdd)
         {
             if (coffeeToAdd == null) throw new Exception("You should not add null entries!");
+            if (!IsCoffeeValid(coffeeToAdd)) throw new Exception("The coffee you are trying to add is not valid");
 
             coffeeModels.Add(coffeeToAdd);
         }
 
         public void DeleteCoffee(Guid coffeeId)
         {
-            throw new NotImplementedException();
+            if(coffeeId == Guid.Empty || coffeeId == null) throw new Exception("Please provide an ID!");
+
+            var coffeeFromList = coffeeModels.FirstOrDefault(x => x.Id == coffeeId);
+            if (coffeeFromList == null) throw new Exception("The coffee you are trying to delete does not exist!");
+
+            coffeeModels.Remove(coffeeFromList);
         }
 
         public List<CoffeeModel> GetCoffees()
         {
             return coffeeModels;
+        }
+
+        private bool IsCoffeeValid(CoffeeModel model)
+        {
+            return model.Id != Guid.Empty && !string.IsNullOrEmpty(model.Name) && model.Price != 0.0f;
         }
     }
 }
--- CoffeeMachineSimulator/CoffeeMachineSimulator.Tests/Services/CoffeeServiceTests.cs
+++ CoffeeMachineSimulator/CoffeeMachineSimulator.Tests/Services/CoffeeServiceTests.cs
@@ -1,5 +1,6 @@
 ﻿using CoffeeMachineSimulator.Services.Models;
 using CoffeeMachineSimulator.Services.Services;
+using FizzWare.NBuilder;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
@@ -37,7 +38,45 @@ namespace CoffeeMachineSimulator.Tests.Services
             Assert.AreEqual(ex.Message, "You should not add null entries!");
         }
 
-        //TODO: Add Unit test for Remaining add cases
-        //Todo: Add Unit test for deleting case
+        [Test]
+        public void AddInvalidCoffee_Throws_NewException()
+        {
+            var coffeeToAdd = Builder<CoffeeModel>.CreateNew()
+                                .With(x => x.Id = Guid.Empty)
+                                .With(x => x.Name = "")
+                                .With(x=>x.Price = 0.0f)
+                                .Build();
+
+            var ex = Assert.Throws<Exception>(() => coffeeService.AddCoffee(coffeeToAdd));
+
+            Assert.AreEqual(ex.Message, "The coffee you are trying to add is not valid");
+        }
+
+        [Test]
+        public void DeleteCoffeeWithEmptyId_Throws_NewException()
+        {
+            var ex = Assert.Throws<Exception>(() => coffeeService.DeleteCoffee(Guid.Empty));
+
+            Assert.AreEqual(ex.Message, "Please provide an ID!");
+        }
+
+        [Test]
+        public void DeleteCoffeeWithNonExistingId_Throws_NewException()
+        {
+            var ex = Assert.Throws<Exception>(() => coffeeService.DeleteCoffee(Guid.NewGuid()));
+
+            Assert.AreEqual(ex.Message, "The coffee you are trying to delete does not exist!");
+        }
+
+        [Test]
+        public void DeleteCoffee_DeletesCoffee()
+        {
+            var initialCountOfCoffees = coffeeService.GetCoffees().Count;
+            var coffeeToRemove = coffeeService.GetCoffees().First();
+
+            coffeeService.DeleteCoffee(coffeeToRemove.Id);
+
+            Assert.AreNotEqual(initialCountOfCoffees, coffeeService.GetCoffees().Count);
+        }
     }
 }
