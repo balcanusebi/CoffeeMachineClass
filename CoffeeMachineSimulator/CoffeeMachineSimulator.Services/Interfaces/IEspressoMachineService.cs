001c82c73c35ab4d33e7663af8769f4733e54d4b
Sebastian Balcanu <sebi_balcanu@yahoo.com>
2020-07-20 19:26:59 +0300

    Added IEspressoMachineService Mocked services

---
+++ CoffeeMachineSimulator/CoffeeMachineSimulator.Services/Interfaces/IEspressoMachineService.cs
@@ -0,0 +1,13 @@
+﻿using CoffeeMachineSimulator.Services.Enums;
+using CoffeeMachineSimulator.Services.Models;
+using System.Collections.Generic;
+
+namespace CoffeeMachineSimulator.Services.Interfaces
+{
+    public interface IEspressoMachineService
+    {
+        CoffeeModel GiveMeACoffee(SweetnessEnum sweetness);
+        float GetSumOfAllCoffees();
+        List<CoffeeModel> MakeAllCoffeesWithSweetness(SweetnessEnum sweetness);
+    }
+}
---
+++ CoffeeMachineSimulator/CoffeeMachineSimulator.Services/Services/EspressoMachineService.cs
@@ -0,0 +1,38 @@
+﻿using CoffeeMachineSimulator.Services.Enums;
+using CoffeeMachineSimulator.Services.Interfaces;
+using CoffeeMachineSimulator.Services.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CoffeeMachineSimulator.Services.Services
+{
+    public class EspressoMachineService : IEspressoMachineService
+    {
+        public ICoffeeService coffeeService;
+
+        public EspressoMachineService(ICoffeeService coffeeService)
+        {
+            this.coffeeService = coffeeService;
+        }
+
+        public float GetSumOfAllCoffees()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public CoffeeModel GiveMeACoffee(SweetnessEnum sweetness)
+        {
+            var listOfCoffees = coffeeService.GetCoffees();
+
+            var myCoffeeToReturn = listOfCoffees.FirstOrDefault(x=>x.Sweetness == sweetness);
+            myCoffeeToReturn.Name = "TestMyCoffee";
+
+            return myCoffeeToReturn;
+        }
+
+        public List<CoffeeModel> MakeAllCoffeesWithSweetness(SweetnessEnum sweetness)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
